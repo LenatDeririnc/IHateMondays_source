fff1a44d853f389aa2b33e7f53306d7b4840823c
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-06 15:04:47 +0300

    refactoring: spawn player flag

--- Assets/Scripts/Services/PlayerService.cs
+++ Assets/Scripts/Services/PlayerService.cs
@@ -1,21 +1,25 @@
 ﻿using Player;
 using Plugins.ServiceLocator;
 using Plugins.ServiceLocator.Interfaces;
+using Scenes;
 using UnityEngine;
 
 namespace Services
 {
     public class PlayerService : Service, IAwakeService
     {
+        [SerializeField] private bool _spawnPlayer = true;
         public PlayerBase Player { get; private set; }
 
-        [SerializeField] private Transform defaultPlayerSpawn;
+        [SerializeField] private PlayerSpawnPointComponent defaultPlayerSpawn;
         private GameFactoryService _factoryService;
 
         public void AwakeService()
         {
             _factoryService = ServiceLocator.Get<GameFactoryService>();
-            Player = _factoryService.SpawnPlayer(defaultPlayerSpawn);
+            
+            if (_spawnPlayer)
+                Player = _factoryService.SpawnPlayer(defaultPlayerSpawn.transform);
         }
     }
 }
