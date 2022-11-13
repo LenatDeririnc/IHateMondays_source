e988d737b0d93189756271e849a3927fb3247a24
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-13 15:37:14 +0300

    zelda blockout

---
+++ Assets/Scripts/Scenes/Room.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Scenes
+{
+    public class Room : MonoBehaviour
+    {
+        [SerializeField] private int _id;
+
+        public int ID => _id;
+
+        [SerializeField] private GameObject _roomMesh;
+
+        public bool isActive { get; private set; } = false;
+
+        public void OnEnterRoom()
+        {
+            _roomMesh.SetActive(true);
+            isActive = true;
+        }
+
+        public void OnExitRoom()
+        {
+            _roomMesh.SetActive(false);
+            isActive = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Services/RoomsService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Plugins.ServiceLocator;
+using Plugins.ServiceLocator.Interfaces;
+using Scenes;
+
+namespace Services
+{
+    public class RoomsService : Service, IAwakeService
+    {
+        public Room[] rooms;
+
+        private Dictionary<int, Room> _roomsByids = new Dictionary<int, Room>();
+
+        private Room _currentRoom;
+
+        public void AwakeService()
+        {
+            foreach (var r in rooms) {
+                _roomsByids[r.ID] = r;
+                if (!r.isActive)
+                    r.OnExitRoom();
+            }
+        }
+
+        public void EnterRoom(int id)
+        {
+            if (_currentRoom != null) {
+                _currentRoom.OnExitRoom();
+            }
+
+            _currentRoom = _roomsByids[id];
+            _currentRoom.OnEnterRoom();
+        }
+    }
+}
