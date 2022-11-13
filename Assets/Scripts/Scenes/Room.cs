4648b633eaf54b0453a151e94f3e4951a74b44f1
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-13 17:28:12 +0300

    zelda: room shader changer, blockout

--- Assets/Scripts/Scenes/Room.cs
+++ Assets/Scripts/Scenes/Room.cs
@@ -1,27 +1,87 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using Services;
+using Unity.VisualScripting;
+using UnityEngine;
 
 namespace Scenes
 {
     public class Room : MonoBehaviour
     {
+        private static readonly int Cutoff = Shader.PropertyToID("_Cutoff");
+        
         [SerializeField] private int _id;
+        [SerializeField] private Transform parentMesh;
 
         public int ID => _id;
-
-        [SerializeField] private GameObject _roomMesh;
+        private RoomsService _service;
+        private List<Material> _materials = new List<Material>();
 
         public bool isActive { get; private set; } = false;
 
+        public void Construct(RoomsService service, bool isActive)
+        {
+            _service = service;
+            var renderers = parentMesh.GetComponentsInChildren<MeshRenderer>();
+
+            foreach (MeshRenderer r in renderers) {
+                foreach (Material mat in r.materials) {
+                    if (!mat.HasFloat(Cutoff))
+                        continue;
+                    _materials.Add(mat);
+                }
+            }
+
+            this.isActive = isActive;
+            if (!isActive)
+                Hide();
+        }
+
         public void OnEnterRoom()
         {
-            _roomMesh.SetActive(true);
+            if (isActive)
+                return;
+            
+            EnterRoom();
             isActive = true;
         }
 
         public void OnExitRoom()
         {
-            _roomMesh.SetActive(false);
+            if (!isActive)
+                return;
+            
+            ExitRoom();
             isActive = false;
+        }
+
+        private void Hide()
+        {
+            foreach (var m in _materials) {
+                m.SetFloat(Cutoff, 1);
+            }
+        }
+
+        private void EnterRoom()
+        {
+            foreach (var m in _materials) {
+                DOTween.To(
+                    () => m.GetFloat(Cutoff), 
+                    value => { m.SetFloat(Cutoff, value); }, 
+                    0,
+                    _service.RoomEnterDuration).SetEase(_service.RoomEnterEase);
+            }
+        }
+
+        private void ExitRoom()
+        {
+            foreach (var m in _materials) {
+                DOTween.To(
+                    () => m.GetFloat(Cutoff), 
+                    value => { m.SetFloat(Cutoff, value); }, 
+                    1,
+                    _service.RoomEnterDuration).SetEase(_service.RoomExitEase);
+            }
         }
     }
 }
--- Assets/Scripts/Services/RoomsService.cs
+++ Assets/Scripts/Services/RoomsService.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
 using Plugins.ServiceLocator;
 using Plugins.ServiceLocator.Interfaces;
 using Scenes;
+using UnityEngine;
 
 namespace Services
 {
     public class RoomsService : Service, IAwakeService
     {
+        [SerializeField] private Room _currentRoom;
+        [SerializeField] public float RoomEnterDuration = 1f;
+        [SerializeField] public Ease RoomEnterEase;
+        [SerializeField] public Ease RoomExitEase;
+        [SerializeField] public float RoomEnterEndValue = -10f;
+
         public Room[] rooms;
 
         private Dictionary<int, Room> _roomsByids = new Dictionary<int, Room>();
 
-        private Room _currentRoom;
 
         public void AwakeService()
         {
             foreach (var r in rooms) {
                 _roomsByids[r.ID] = r;
-                if (!r.isActive)
-                    r.OnExitRoom();
+                r.Construct(this, r.ID == _currentRoom.ID);
             }
         }
 
         public void EnterRoom(int id)
         {
+            if (_currentRoom.ID == id)
+                return;
+            
             if (_currentRoom != null) {
                 _currentRoom.OnExitRoom();
             }
