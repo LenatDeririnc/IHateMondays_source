3d66e567653e0690b7afad683ceb337e7043eac2
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-13 17:44:10 +0300

    zelda: switch to next scene

---
+++ Assets/Scripts/Scenes/Props/SwitchSceneInteractable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Plugins.ServiceLocator;
+using Props;
+using SceneManager.ScriptableObjects;
+using Services;
+using UnityEngine;
+
+namespace Scenes.Props
+{
+    public class SwitchSceneInteractable: MonoBehaviour, IInteractable
+    {
+        [SerializeField] private SceneLink _switchScene;
+        private SceneLoadingService _sceneLoadingService;
+
+        private void Awake()
+        {
+            _sceneLoadingService = ServiceLocator.Get<SceneLoadingService>();
+        }
+
+        public void Interact()
+        {
+            _sceneLoadingService.LoadScene(_switchScene);
+        }
+
+        public bool IsAvailableToInteract()
+        {
+            return true;
+        }
+    }
+}
