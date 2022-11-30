2b8329b86e7fcb2a748a2d3090dd09177679e7af
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-30 22:58:45 +0300

    vn: end

--- Assets/Scripts/Services/VisualNovelService.cs
+++ Assets/Scripts/Services/VisualNovelService.cs
@@ -1,6 +1,7 @@
 ﻿using Fungus;
 using Plugins.ServiceLocator;
 using Plugins.ServiceLocator.Interfaces;
+using SceneManager.ScriptableObjects;
 using UnityEngine;
 
 namespace Services
@@ -10,10 +11,18 @@ namespace Services
         [SerializeField] private Stage _stage;
         [SerializeField] private Flowchart _flowchart;
         [SerializeField] private string _blockNameStart = "Start";
+        [SerializeField] private SceneLink _endScene;
+        private SceneLoadingService _sceneLoader;
         public Stage Stage => _stage;
         public void AwakeService()
         {
+            _sceneLoader = ServiceLocator.Get<SceneLoadingService>();
             _flowchart.ExecuteBlock(_blockNameStart);
+        }
+
+        public void NextScene()
+        {
+            _sceneLoader.LoadScene(_endScene);
         }
     }
 }
