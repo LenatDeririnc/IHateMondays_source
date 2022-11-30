a722ceb5de6fa4f7b25cb75c002797cac920ae8d
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-11-30 22:43:22 +0300

    vn: pre end

--- Assets/Scripts/Services/VisualNovelService.cs
+++ Assets/Scripts/Services/VisualNovelService.cs
@@ -1,12 +1,19 @@
 ﻿using Fungus;
 using Plugins.ServiceLocator;
+using Plugins.ServiceLocator.Interfaces;
 using UnityEngine;
 
 namespace Services
 {
-    public class VisualNovelService : Service
+    public class VisualNovelService : Service, IAwakeService
     {
         [SerializeField] private Stage _stage;
+        [SerializeField] private Flowchart _flowchart;
+        [SerializeField] private string _blockNameStart = "Start";
         public Stage Stage => _stage;
+        public void AwakeService()
+        {
+            _flowchart.ExecuteBlock(_blockNameStart);
+        }
     }
 }
